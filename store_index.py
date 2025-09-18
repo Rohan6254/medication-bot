2118259f6e9493576b5d9e45401add8fefbf2091
Rohan ganji <ganjirohan007@gmail.com>
2025-09-18 15:06:54 +0530

    Updated chatbot with new prompt and bug fixes

---
+++ store_index.py
@@ -0,0 +1,43 @@
+from dotenv import load_dotenv
+import os
+from src.helper import load_pdf_files,filter_to_minimal_docs,text_split,download_embeddings
+from pinecone import Pinecone 
+from langchain_pinecone import PineconeVectorStore
+from pinecone import ServerlessSpec
+
+
+
+
+load_dotenv()
+PINECONE_API_KEY=os.getenv("PINECONE_API_KEY")
+
+os.environ["PINECONE_API_KEY"]=PINECONE_API_KEY
+
+
+pinecone_api_key =PINECONE_API_KEY
+pc=Pinecone(api_key=pinecone_api_key)
+
+extracted_data = load_pdf_files("data")
+minimal_docs = filter_to_minimal_docs(extracted_data)
+texts_chunk=text_split(minimal_docs)
+
+embeddings=download_embeddings()
+
+
+index_name = "medical-chatbot"
+if not pc.has_index(index_name):
+    pc.create_index(
+        name=index_name,
+        dimension=384,
+        metric="cosine",
+        spec=ServerlessSpec(cloud="aws",region="us-east-1")
+    )
+index = pc.Index(index_name)
+
+
+docsearch = PineconeVectorStore.from_documents(
+    documents=texts_chunk,
+    embedding=embeddings,
+    index_name=index_name
+)
+
